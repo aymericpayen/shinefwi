4cbf03e85fd67a0d031ac27c6c246090e70cb71d
Aymeric Payen <aymeric.payen@gmail.com>
2022-03-15 15:51:30 +0100

    slide show added to item show, but image won't display

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @items = Item.all
   end
 
   def show
-    @items = Item.all
+    @items = Item.find(params[:id])
   end
 end
